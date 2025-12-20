b949cbd125260fdff95997db658256f8b6c0f2f4
Lukáš Hejnal <kejhy93@gmail.com>
2025-12-21 00:27:25 +0100

    Init commit

---
+++ src/main/java/org/hejnaluk/virtual_threads_structured_concurency/VirtualThreadsStructuredConcurencyApplication.java
@@ -0,0 +1,13 @@
+package org.hejnaluk.virtual_threads_structured_concurency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualThreadsStructuredConcurencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualThreadsStructuredConcurencyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hejnaluk/virtual_threads_structured_concurency/VirtualThreadsStructuredConcurencyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hejnaluk.virtual_threads_structured_concurency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualThreadsStructuredConcurencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
